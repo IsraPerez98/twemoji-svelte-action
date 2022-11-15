f0563c836cdc609ccc5f2c234b3575f94f4b40e1
IsraPerez98 <isra1998isra@gmail.com>
2022-11-15 20:54:05 -0300

    🧹 Clean up more code before submitting

--- src/__tests__/SvelteTest.test.ts
+++ src/__tests__/SvelteTest.test.ts
@@ -1,6 +1,6 @@
 /**
-* @jest-environment jsdom
-*/
+ * @jest-environment jsdom
+ */
 
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/svelte';
@@ -9,43 +9,42 @@ import { twemoji } from '../index';
 
 import Component from './SvelteTest.svelte';
 
-
 test('Renders message with default emoji', () => {
-    const emoji = '🚀';
-    const message = 'Hello world! ' + emoji;
-    
-    //@ts-ignore check github.com/testing-library/svelte-testing-library/issues/194
-    const { getByText } = render(Component, {
-        props: {
-            message: message,
-        }
-    });
+  const emoji = '🚀';
+  const message = 'Hello world! ' + emoji;
 
-    expect(getByText(message)).toBeInTheDocument();
+  //@ts-ignore check github.com/testing-library/svelte-testing-library/issues/194
+  const { getByText } = render(Component, {
+    props: {
+      message: message,
+    },
+  });
+
+  expect(getByText(message)).toBeInTheDocument();
 });
 
 test('Renders message with tweemoji', () => {
-    const emoji = '🚀';
-    const tweemojiCode = "1f680";
+  const emoji = '🚀';
+  const tweemojiCode = '1f680';
 
-    const message = 'Hello world! ' + emoji;
-    
-    //@ts-ignore check github.com/testing-library/svelte-testing-library/issues/194
-    render(Component, {
-        props: {
-            message: message,
-            action: twemoji,
-        }
-    });
+  const message = 'Hello world! ' + emoji;
 
-    //grab img with alt text of emoji
-    const img = screen.getByAltText(emoji);
-    expect(img).toBeInTheDocument();
-    
-    expect(img).toHaveClass('emoji');
-    expect(img).toHaveAttribute('src');
+  //@ts-ignore check github.com/testing-library/svelte-testing-library/issues/194
+  render(Component, {
+    props: {
+      message: message,
+      action: twemoji,
+    },
+  });
 
-    //check if src is correct
-    const src = img.getAttribute('src');
-    expect(src).toContain(tweemojiCode + '.png');
+  //grab img with alt text of emoji
+  const img = screen.getByAltText(emoji);
+  expect(img).toBeInTheDocument();
+
+  expect(img).toHaveClass('emoji');
+  expect(img).toHaveAttribute('src');
+
+  //check if src is correct
+  const src = img.getAttribute('src');
+  expect(src).toContain(tweemojiCode + '.png');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,57 @@
-//Importing this normally will cause an error
+// Importing this normally will cause an error
+// tslint:disable-next-line:no-var-requires
 const twemojify = require('twemoji');
+
 import { afterUpdate } from 'svelte';
 
 declare type ParseCallback = (icon: string, options: object, variant: string) => string | false;
 
 declare interface TwemojiOptions {
-    /**
-    * Default: MaxCDN
-    */
-    base?: string;
-    /**
-    * Default: .png
-    */
-    ext?: string;
-    /**
-    * Default: emoji
-    */
-    className?: string;
-    /**
-    * Default: 72x72
-    */
-    size?: string | number;
-    /**
-    * To render with SVG use `folder: svg, ext: .svg`
-    */
-    folder?: string;
-    /**
-    * The function to invoke in order to generate image src(s).
-    */
-    callback?: ParseCallback
-    /**
-    * The function to invoke in order to generate additional, custom attributes for the image tag.
-    * Default () => ({})
-    * @param icon the lower case HEX code point i.e. "1f4a9"
-    * @param variant variant the optional \uFE0F ("as image") variant, in case this info is anyhow meaningful. By default this is ignored.
-    * 
-    */
-    attributes?(icon: string, variant: string): object;
+  /**
+   * Default: MaxCDN
+   */
+  base?: string;
+  /**
+   * Default: .png
+   */
+  ext?: string;
+  /**
+   * Default: emoji
+   */
+  className?: string;
+  /**
+   * Default: 72x72
+   */
+  size?: string | number;
+  /**
+   * To render with SVG use `folder: svg, ext: .svg`
+   */
+  folder?: string;
+  /**
+   * The function to invoke in order to generate image src(s).
+   */
+  callback?: ParseCallback;
+  /**
+   * The function to invoke in order to generate additional, custom attributes for the image tag.
+   * Default () => ({})
+   * @param icon the lower case HEX code point i.e. "1f4a9"
+   * @param variant variant the optional \uFE0F ("as image") variant, in case this info is anyhow meaningful. By default this is ignored.
+   *
+   */
+  attributes?(icon: string, variant: string): object;
 }
 
 type TwemojifyOptions = TwemojiOptions | ParseCallback;
 
 export function twemoji(node: HTMLElement, options: TwemojifyOptions = {}) {
+  twemojify.parse(node, options);
+
+  afterUpdate(() => {
     twemojify.parse(node, options);
-    
-    afterUpdate(() => {
-        twemojify.parse(node, options);
-    });
-    return {
-        update() {
-            console.warn("Can't change twemoji options with an update.");
-        }
-    }
+  });
+  return {
+    update() {
+      console.warn("Can't change twemoji options with an update.");
+    },
+  };
 }
