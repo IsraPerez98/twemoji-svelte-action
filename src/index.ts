ec751e99e9730a7bb595289a46005ce9cd56cd8c
IsraPerez98 <isra1998isra@gmail.com>
2022-11-15 20:44:16 -0300

    🧹 Tidy up things

--- src/index.ts
+++ src/index.ts
@@ -4,52 +4,52 @@ import { afterUpdate } from 'svelte';
 
 declare type ParseCallback = (icon: string, options: object, variant: string) => string | false;
 
-type TwemojifyOptions = TwemojiOptions | ParseCallback;
-
 declare interface TwemojiOptions {
     /**
-     * Default: MaxCDN
-     */
+    * Default: MaxCDN
+    */
     base?: string;
     /**
-     * Default: .png
-     */
+    * Default: .png
+    */
     ext?: string;
     /**
-     * Default: emoji
-     */
+    * Default: emoji
+    */
     className?: string;
     /**
-     * Default: 72x72
-     */
+    * Default: 72x72
+    */
     size?: string | number;
     /**
-     * To render with SVG use `folder: svg, ext: .svg`
-     */
+    * To render with SVG use `folder: svg, ext: .svg`
+    */
     folder?: string;
     /**
-     * The function to invoke in order to generate image src(s).
-     */
+    * The function to invoke in order to generate image src(s).
+    */
     callback?: ParseCallback
     /**
-     * The function to invoke in order to generate additional, custom attributes for the image tag.
-     * Default () => ({})
-     * @param icon the lower case HEX code point i.e. "1f4a9"
-     * @param variant variant the optional \uFE0F ("as image") variant, in case this info is anyhow meaningful. By default this is ignored.
-     * 
-     */
+    * The function to invoke in order to generate additional, custom attributes for the image tag.
+    * Default () => ({})
+    * @param icon the lower case HEX code point i.e. "1f4a9"
+    * @param variant variant the optional \uFE0F ("as image") variant, in case this info is anyhow meaningful. By default this is ignored.
+    * 
+    */
     attributes?(icon: string, variant: string): object;
-  }
+}
+
+type TwemojifyOptions = TwemojiOptions | ParseCallback;
 
 export function twemoji(node: HTMLElement, options: TwemojifyOptions = {}) {
     twemojify.parse(node, options);
-
+    
     afterUpdate(() => {
         twemojify.parse(node, options);
     });
-	return {
-		update() {
+    return {
+        update() {
             console.warn("Can't change twemoji options with an update.");
         }
-	}
+    }
 }
