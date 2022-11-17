786bc6429bf17008c2ab8f02a573a72d5882210d
IsraPerez98 <isra1998isra@gmail.com>
2022-11-16 23:16:23 -0300

    🪲 Bugfix Fn called outside component init

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,4 @@ type TwemojifyOptions = TwemojiOptions | ParseCallback;
 
 export function twemoji(node: HTMLElement, options: TwemojifyOptions = {}) {
   twemojify.parse(node, options);
-
-  afterUpdate(() => {
-    twemojify.parse(node, options);
-  });
-  return {
-    update() {
-      console.warn("Can't change twemoji options with an update.");
-    },
-  };
 }
